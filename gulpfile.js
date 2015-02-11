efef8c3a5e626fd8a84928d8ae046a561582375f
ashleycartercreates <hello@ashleycartercreates.com>
2015-02-11 12:54:36 -0700

    Beginning nowmedia templating

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp'),
+    sass = require('gulp-sass');
+
+//Sass
+gulp.task('sass', function() {
+    gulp.src('sass/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('css/'));
+});
+
+//Watch tasks
+gulp.task('watch', function() {
+    gulp.watch('sass/*.scss', ['sass']);
+});
+
+gulp.task('default', ['sass', 'watch']);
